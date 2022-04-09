c0e95ba36365207ef3d952282c0351c851574250
Cix8 <francesco.natal_fn@libero.it>
2022-04-09 16:37:33 +0200

    add others questions, add finalResult, add animation_colorzzz

--- js/script.js
+++ js/script.js
@@ -58,25 +58,25 @@ startBtn.addEventListener ('click',
 
         console.log(userName.value);
 
-        solving = 'Python';
+        solving = '8 giorni';
         
-        quest.innerHTML = 'Vediamo se mi stai simpatico, scegli uno tra questi linguaggi di programmazione:'
+        quest.innerHTML = 'Una lumaca si trova in un pozzo di 10 metri. Ogni giorno sale di 3 metri, ma ogni notte ne scende di 2. In quanti giorni riuscirà ad uscire?'
         console.log(quest);
 
-        answ1.value = 'Python';
-        answ1Label.innerHTML = 'Python';
+        answ1.value = '8 giorni';
+        answ1Label.innerHTML = '8 giorni';
         console.log(answ1.value);
 
-        answ2.value = 'Java';
-        answ2Label.innerHTML = 'Java';
+        answ2.value = '4 giorni';
+        answ2Label.innerHTML = '4 giorni';
         console.log(answ2.value);
 
-        answ3.value = 'JS';
-        answ3Label.innerHTML = 'Javascript';
+        answ3.value = '10 giorni';
+        answ3Label.innerHTML = '10 giorni';
         console.log(answ3.value);
 
-        answ4.value = 'PHP';
-        answ4Label.innerHTML = 'PHP';
+        answ4.value = '7 giorni';
+        answ4Label.innerHTML = '7 giorni';
         console.log(answ4.value);
     }
 )
@@ -96,6 +96,8 @@ const nextBtn3 = document.getElementById('next-btn3');
 const nextBtn4 = document.getElementById('next-btn4');
 
 const nextBtn5 = document.getElementById('next-btn5');
+
+const nextBtn6 = document.getElementById('next-btn6');
 
 nextBtn.addEventListener ('click',
     function () {
@@ -213,20 +215,82 @@ nextBtn3.addEventListener ('click',
     }
 )
 
+nextBtn4.addEventListener ('click',
+    function () {
+        submitBtn.classList.remove('d-none');
+        nextBtn4.classList.add('d-none');
+        nextBtn5.classList.remove('d-none');
+        
+        solving = '28 metri';
+
+        quest.innerHTML = `Un plotone di soldati è composto da 3 colonne e 15 righe. Le righe sono distanti tra loro di 2 metri. Quanto è lungo il plotone?`;
+        console.log(quest);
+
+        answ1.value = '14 metri';
+        answ1Label.innerHTML = '14 metri';
+        console.log(answ1.value);
+
+        answ2.value = '15 metri';
+        answ2Label.innerHTML = '15 metri';
+        console.log(answ2.value);
+
+        answ3.value = '30 metri';
+        answ3Label.innerHTML = '30 metri';
+        console.log(answ3.value);
+
+        answ4.value = '28 metri';
+        answ4Label.innerHTML = '28 metri';
+        console.log(answ4.value);
+    }
+)
+
+nextBtn5.addEventListener ('click', 
+    function () {
+        submitBtn.classList.remove('d-none');
+        nextBtn5.classList.add('d-none');
+        nextBtn6.classList.remove('d-none');
+        
+        solving = 'Tutti';
+
+        quest.innerHTML = `Quanti mesi hanno 28 giorni?`;
+        console.log(quest);
+
+        answ1.value = 'Nessuno';
+        answ1Label.innerHTML = 'Nessuno';
+        console.log(answ1.value);
+
+        answ2.value = 'Uno';
+        answ2Label.innerHTML = 'Uno';
+        console.log(answ2.value);
+
+        answ3.value = 'Tutti';
+        answ3Label.innerHTML = 'Tutti';
+        console.log(answ3.value);
+
+        answ4.value = 'Due';
+        answ4Label.innerHTML = 'Due';
+        console.log(answ4.value);
+    }
+)
+
 submitBtn.addEventListener ('click',
     function () {
-        if (answ1.checked == true && answ1.value === 'Python' && score < 1) {
+        if (answ1.checked == true && answ1.value === solving && score < 1) {
             ++score;
             console.log(score);
-        } else if (answ3.checked == true && answ3.value === '1' && score < 2) {
+        } else if (answ3.checked == true && answ3.value === solving && score < 2) {
             ++score;
             console.log(score);
-        } else if (answ2.checked == true && answ2.value === '221 anni' && score < 3) {
+        } else if (answ2.checked == true && answ2.value === solving && score < 3) {
             ++score;
             console.log(score);
-        } else if (answ1.checked && answ1.value ==='20' && score < 4) {
+        } else if (answ1.checked && answ1.value === solving && score < 4) {
             ++score;
-        } else if (answ3.checked && answ3.value === '1' && score < 5) {
+        } else if (answ3.checked && answ3.value === solving && score < 5) {
+            ++score;
+        } else if (answ4.checked && answ4.value === solving && score < 6) {
+            ++score;
+        } else if (answ3.checked && answ3.value === solving && score < 7) {
             ++score;
         } else {
             --score;
@@ -235,5 +299,21 @@ submitBtn.addEventListener ('click',
         submitBtn.classList.add('d-none');
 
         scoreDisplay.innerHTML = `Punteggio: ${score}`;
+    }
+)
+
+let finalResult = document.getElementById('final-result');
+
+nextBtn6.addEventListener ('click',
+    function () {
+        nextBtn6.classList.add('d-none');
+        quest.classList.add('d-none');
+        finalResult.classList.remove('d-none');
+
+        scoreDisplay.classList.add('d-none');
+        document.querySelector('.questionary .container').classList.add('d-none');
+        document.querySelector('.questionary .btn-container').classList.add('d-none');
+
+        finalResult.innerHTML = `Complimenti ${nickName.value}!!! Il tuo punteggio è di <span class='d-block'>${score}</span>`
     }
 )
